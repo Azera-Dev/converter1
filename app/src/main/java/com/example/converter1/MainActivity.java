e9348bf84e412fc9e933289b49b306d01dee33da
azera-dev <reza.hosseinzadeh.mk@gmail.com>
2019-02-28 16:43:46 +0330

    update

--- app/src/main/java/com/example/converter1/MainActivity.java
+++ app/src/main/java/com/example/converter1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.converter1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -21,37 +23,58 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
+    public byte convTypeFlag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Enter Your date Of Birth", Toast.LENGTH_SHORT).show();
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(this);
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.j2g: {
+                convTypeFlag = 0;
+                break;
+            }
+            case R.id.g2j: {
+                convTypeFlag = 1;
+                break;
+            }
+        }
+        return true;
+    }
+
+    @Override
     public void onClick(View v) {
-        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
         try {
-            EditText editTextDate =(EditText)findViewById(R.id.edit_text_date);
+            EditText editTextDate = (EditText) findViewById(R.id.edit_text_date);
             TextView textResult = (TextView) findViewById(R.id.textViewResult);
+            TextView edtxtConvState = (TextView) findViewById(R.id.converter_state);
             String dateStr = editTextDate.getText().toString();
             String[] splitStr = dateStr.split("/");
             int year = Integer.parseInt(splitStr[0]);
             int month = Integer.parseInt(splitStr[1]);
             int day = Integer.parseInt(splitStr[2]);
-            if (toggleButton.isChecked()) {
-                textResult.setText(Arrays.toString(Convert.jalali_to_gregorian(year, month, day))+("Cristyan"));
+            if (convTypeFlag == 0) {
+                textResult.setText(Arrays.toString(Convert.jalali_to_gregorian(year, month, day)) + ("Cristian"));
 
-            } else {
-                textResult.setText(Arrays.toString(Convert.gregorian_to_jalali(year,month,day))+("Jalali"));
+            } else if (convTypeFlag == 1) {
+                textResult.setText(Arrays.toString(Convert.gregorian_to_jalali(year, month, day)) + ("Jalali"));
             }
 
         } catch (Exception e) {
             Log.e(TAG, "Exception Logger failed!", e);
-
         }
-
     }
 }
